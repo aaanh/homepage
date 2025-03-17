66a65bd2562eee460b22cafaeece03f7a01e0a39
Anh Nguyen <anhnguyen@aaanh.com>
2025-03-17 19:13:14 -0400

    Carousel

--- src/components/sections/project.tsx
+++ src/components/sections/project.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import getProjects from "@/data/projects";
 import SectionHeader from "./section-header";
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
+import Autoscroll from "embla-carousel-auto-scroll";
 
 export default function Project() {
   const projects = getProjects();
@@ -12,22 +14,29 @@ export default function Project() {
   return (
     <section className="flex flex-col gap-4 p-2">
       <SectionHeader title="Personal Projects" />
-      <div className="flex flex-wrap justify-between gap-4">
-        {projects.map((project, idx) => (
-          <Card
-            key={project.title + "-" + idx}
-            className="hover:bg-accent/50 border-none rounded-none w-[350px] transition-all ease-in-out hover:cursor-pointer"
-            onClick={() => router.push(project.link_ref ?? "#")}
-          >
-            <CardHeader>
-              <CardTitle>{project.title}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>{project.stack}</p>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
+
+      <Carousel plugins={[Autoscroll()]} opts={{ align: "start", loop: true }}>
+        <CarouselContent>
+          {projects.map((project, idx) => (
+            <CarouselItem
+              className="lg:basis-1/4 basis-1/2"
+              key={project.title + "-" + idx}
+            >
+              <Card
+                className="hover:bg-accent/50 border-none rounded-none w-[350px] min-h-56 transition-all ease-in-out hover:cursor-pointer"
+                onClick={() => router.push(project.link_ref ?? "#")}
+              >
+                <CardHeader>
+                  <CardTitle>{project.title}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>{project.stack}</p>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </section>
   );
 }
