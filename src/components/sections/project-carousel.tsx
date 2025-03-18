4d4a5c67b6413a209613875961f93db5bc613d4e
Anh Nguyen <anhnguyen@aaanh.com>
2025-03-17 22:21:49 -0400

    Fix carousel layout

---
+++ src/components/sections/project-carousel.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import getProjects from "@/data/projects";
+import { useRouter } from "next/navigation";
+import { Card, CardHeader, CardTitle, CardContent } from "../ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  type CarouselApi,
+} from "../ui/carousel";
+import Autoscroll from "embla-carousel-auto-scroll";
+import { useState, useEffect } from "react";
+
+export default function ProjectCarousel() {
+  const projects = getProjects();
+  const router = useRouter();
+
+  return (
+    <div className="relative">
+      <Carousel
+        plugins={[
+          Autoscroll({
+            speed: 1.5,
+            stopOnInteraction: false,
+          }),
+        ]}
+        opts={{ align: "start", loop: true }}
+      >
+        <CarouselContent>
+          {projects.map((project, idx) => (
+            <CarouselItem
+              className="lg:basis-1/4"
+              key={project.title + "-" + idx}
+            >
+              <Card
+                className="hover:bg-accent/50 border-none rounded-none w-[350px] min-h-56 transition-all ease-in-out hover:cursor-pointer"
+                onClick={() => router.push(project.link_ref ?? "#")}
+              >
+                <CardHeader>
+                  <CardTitle>{project.title}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>{project.stack}</p>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </div>
+  );
+}
--- src/components/sections/project.tsx
+++ src/components/sections/project.tsx
@@ -1,42 +1,11 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
-import getProjects from "@/data/projects";
+import ProjectCarousel from "./project-carousel";
 import SectionHeader from "./section-header";
-import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
-import Autoscroll from "embla-carousel-auto-scroll";
 
 export default function Project() {
-  const projects = getProjects();
-  const router = useRouter();
-
   return (
     <section className="flex flex-col gap-4 p-2">
       <SectionHeader title="Personal Projects" />
-
-      <Carousel plugins={[Autoscroll()]} opts={{ align: "start", loop: true }}>
-        <CarouselContent>
-          {projects.map((project, idx) => (
-            <CarouselItem
-              className="lg:basis-1/4 basis-1/2"
-              key={project.title + "-" + idx}
-            >
-              <Card
-                className="hover:bg-accent/50 border-none rounded-none w-[350px] min-h-56 transition-all ease-in-out hover:cursor-pointer"
-                onClick={() => router.push(project.link_ref ?? "#")}
-              >
-                <CardHeader>
-                  <CardTitle>{project.title}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <p>{project.stack}</p>
-                </CardContent>
-              </Card>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
+      <ProjectCarousel />
     </section>
   );
 }
