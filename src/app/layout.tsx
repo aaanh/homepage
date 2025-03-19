939efdd31d7543d8d32d1ecd6764421f56b87e30
Anh Nguyen <anhnguyen@aaanh.com>
2025-03-19 00:43:32 -0400

    Add Speed Insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { ThemeProvider } from "../components/theme-provider";
 import "@/styles/globals.css";
 import { geistSans, geistMono } from "@/fonts";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "AAANH",
@@ -29,6 +29,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         >
           {children}
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
