785d53ec1ac46d53a04a90d61e2a56a25b32c54f
Anh Nguyen <anhnguyen@aaanh.com>
2025-05-03 20:05:40 -0400

    Remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import CallToAction from "@/components/sections/call-to-action";
 import Education from "@/components/sections/education";
 import Experience from "@/components/sections/experience";
 import Footer from "@/components/sections/footer";
-import { GithubFeed } from "@/components/sections/feeds/containers/github-feed";
 import Header from "@/components/sections/header";
 import Hero from "@/components/sections/hero";
 import Project from "@/components/sections/project";
